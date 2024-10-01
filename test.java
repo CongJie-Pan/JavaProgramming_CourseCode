8825d000f36e938e1b51c4ca2a2888a724ef5c7c
Jay Pan <smartjay1206@gmail.com>
2024-10-01 21:31:14 +0800

    test commit

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-			
-        Scanner scannjer = new Scanner(System.in);
-        System.out.print("What is your name：");
-        String inputName = scanner.next();
-        System.out.print("\n Hello ! " + inputName);
-    }
-}
